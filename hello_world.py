12dc4341ca00d681989f773b7486b4933f3caacc
Mikko <mikko.luhtasaari@tuni.fi>
2020-01-17 13:48:06 +0200

    fix typo
    qwerty
    
    asd

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
+print("Not another hello world")
+print("No")
+print("More")
+print("Hello")
+print("World")
+print("Please, I'm begging you")
 print("Hello world!")
+print("Not another hello world")
+
